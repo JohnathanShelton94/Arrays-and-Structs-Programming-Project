f3b4fb633daa508d59f5e13b25acd251f9701049
Johnathan Shelton <js24634@email.vccs.edu>
2024-10-16 21:28:26 -0400

    Add project files.

---
+++ Arrays and Structs Programming Project.cpp
@@ -0,0 +1,9 @@
+// Arrays and Structs Programming Project.cpp : This file contains the 'main' branch of the Arrays and Structs Programming Project
+//
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Arrays and Structs Programming Project!\n";
+}
